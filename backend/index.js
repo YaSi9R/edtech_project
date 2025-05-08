c4e3f2cce2e6ffd3f58a5e7cf270ffe5346515ab
mohd_yasir <mohdarman2369@gmail.com>
2025-05-08 21:10:38 +0530

    adding final result

--- backend/index.js
+++ backend/index.js
@@ -29,7 +29,7 @@ const allowedOrigins = [
 
 app.use(
     cors({
-        origin: allowedOrigins,
+        origin: "https://studyspheree.vercel.app",
         methods: ["GET", "POST", "PUT", "DELETE"],
         credentials: true,
     })
