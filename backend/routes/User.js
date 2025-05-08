cd15e68de293303aa4020f60f6670cd046a3177d
mohd_yasir <mohdarman2369@gmail.com>
2025-05-08 19:16:32 +0530

    deleting resetPassword functionality

--- backend/routes/User.js
+++ backend/routes/User.js
@@ -10,7 +10,7 @@ const {
   changePassword,
 } = require("../controllers/Auth")
 
-const{resetPassword}=require("../controllers/resetPassword")
+// const{resetPassword}=require("../controllers/resetPassword")
 
 const { auth } = require("../middleware/auth")
 
@@ -37,10 +37,10 @@ router.post("/changepassword", auth, changePassword)
 // ********************************************************************************************************
 
 // Route for generating a reset password token
-router.post("/reset-password-token", resetPasswordToken)
+// router.post("/reset-password-token", resetPasswordToken)
 
 // Route for resetting user's password after verification
-router.post("/reset-password", resetPassword)
+// router.post("/reset-password", resetPassword)
 
 // Export the router for use in the main application
 module.exports = router
