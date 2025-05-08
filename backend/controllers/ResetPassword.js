14bb8a708771c2120f20e345668950cc1740ab8d
mohd_yasir <mohdarman2369@gmail.com>
2025-05-08 19:10:46 +0530

    updating rest

--- backend/controllers/ResetPassword.js
+++ backend/controllers/ResetPassword.js
@@ -2,50 +2,50 @@ const User = require("../models/User")
 const mailSender = require("../utils/mailSender")
 const bcrypt = require("bcrypt")
 const crypto = require("crypto")
-exports.resetPasswordToken = async (req, res) => {
-  try {
-    const email = req.body.email
-    const user = await User.findOne({ email: email })
-    if (!user) {
-      return res.json({
-        success: false,
-        message: `This Email: ${email} is not Registered With Us Enter a Valid Email `,
-      })
-    }
-    const token = crypto.randomBytes(20).toString("hex")
+// exports.resetPasswordToken = async (req, res) => {
+//   try {
+//     const email = req.body.email
+//     const user = await User.findOne({ email: email })
+//     if (!user) {
+//       return res.json({
+//         success: false,
+//         message: `This Email: ${email} is not Registered With Us Enter a Valid Email `,
+//       })
+//     }
+//     const token = crypto.randomBytes(20).toString("hex")
 
-    const updatedDetails = await User.findOneAndUpdate(
-      { email: email },
-      {
-        token: token,
-        resetPasswordExpires: Date.now() + 3600000,
-      },
-      { new: true }
-    )
-    console.log("DETAILS", updatedDetails)
+//     const updatedDetails = await User.findOneAndUpdate(
+//       { email: email },
+//       {
+//         token: token,
+//         resetPasswordExpires: Date.now() + 3600000,
+//       },
+//       { new: true }
+//     )
+//     console.log("DETAILS", updatedDetails)
 
-    // const url = `http://localhost:3000/update-password/${token}`
-    const url = `https://studynotion-edtech-project.vercel.app/update-password/${token}`
+//     // const url = `http://localhost:3000/update-password/${token}`
+//     const url = `https://studynotion-edtech-project.vercel.app/update-password/${token}`
 
-    await mailSender(
-      email,
-      "Password Reset",
-      `Your Link for email verification is ${url}. Please click this url to reset your password.`
-    )
+//     await mailSender(
+//       email,
+//       "Password Reset",
+//       `Your Link for email verification is ${url}. Please click this url to reset your password.`
+//     )
 
-    res.json({
-      success: true,
-      message:
-        "Email Sent Successfully, Please Check Your Email to Continue Further",
-    })
-  } catch (error) {
-    return res.json({
-      error: error.message,
-      success: false,
-      message: `Some Error in Sending the Reset Message`,
-    })
-  }
-}
+//     res.json({
+//       success: true,
+//       message:
+//         "Email Sent Successfully, Please Check Your Email to Continue Further",
+//     })
+//   } catch (error) {
+//     return res.json({
+//       error: error.message,
+//       success: false,
+//       message: `Some Error in Sending the Reset Message`,
+//     })
+//   }
+// }
 
 exports.resetPassword = async (req, res) => {
   try {
