b708052712c1744b589e95e41eadd75fc354d0cd
mohd_yasir <mohdarman2369@gmail.com>
2026-01-03 16:51:18 +0530

    Remove .env from repository

--- backend/index.js
+++ backend/index.js
@@ -29,7 +29,7 @@ const allowedOrigins = [
 
 app.use(
     cors({
-        origin: "https://studyspheree-iota.vercel.app",
+        origin: "http://localhost:3000",
         methods: ["GET", "POST", "PUT", "DELETE"],
         credentials: true,
     })
